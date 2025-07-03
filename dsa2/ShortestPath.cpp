e012929f1538589f131aa1e67b774ec53fa5dc39
SandstromPL <paritoshlahre@gmail.com>
2025-07-04 00:10:17 +0530

    completed topoSort approach

--- dsa2/ShortestPath.cpp
+++ dsa2/ShortestPath.cpp
@@ -7,16 +7,17 @@ class shortPath {
             vis[node] = 1;
             for(auto it : adj[node]){
                 if(!vis[it.first]){
-
+                    topoDfs(it.first, vis, st, adj);
                 }
             }
+            st.push(node);
         }
     public:
         // Function to find the shortest path in a graph using topological sorting
         // 1 -> topological sort the graph
         // 2 -> relax the edges in topological order
         // 3 -> return the distance array
-        vector<int> shortestPath(int V, vector<vector<int>> &edges){
+        vector<int> shortestPath(int V, vector<vector<int>> &edges, int source){
             vector<vector<pair<int,int>>> adj;
             for(int i = 0; i < V; i++){
                 int u = edges[i][0];
@@ -24,6 +25,8 @@ class shortPath {
                 int w = edges[i][2];
                 adj[u].push_back({v,w});
             }
+
+            //step 1
             int vis[V] = {0};
             stack<int> st;
             
@@ -32,6 +35,22 @@ class shortPath {
                     topoDfs(i, vis, st, adj);
                 }
             }
+
+            //step 2
+            vector<int> dist(V, INT_MAX);
+            dist[source] = 0;
+            while(!st.empty()){
+                int node = st.top();
+                st.pop();
+                for(auto it : adj[node]){
+                    int v = it.first;
+                    int w = it.second;
+                    if(dist[node] + w < dist[v]){
+                        dist[v] = dist[node] + w;
+                    }
+                }
+            }
+            return dist;
         }
 
 };
